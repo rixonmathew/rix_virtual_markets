5d78f70c1509946a73a86c66ee01d2345fe284a4
Rixon Mathew <rixonmathew@gmail.com>
2013-08-19 16:07:03 +0530

    1) Updated comment for Online tests

--- exchange/src/test/java/com/rixon/virtualmarket/exchange/order/controller/OrderServiceTests.java
+++ exchange/src/test/java/com/rixon/virtualmarket/exchange/order/controller/OrderServiceTests.java
@@ -4,6 +4,7 @@ import com.alibaba.fastjson.JSON;
 import com.rixon.virtualmarket.exchange.order.domain.Order;
 import com.rixon.virtualmarket.exchange.order.domain.OrderResponse;
 import com.rixon.virtualmarket.util.TestUtil;
+import org.junit.Ignore;
 import org.springframework.web.client.RestTemplate;
 
 import org.junit.Test;
@@ -12,7 +13,8 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 /**
- * This class is used for testing the RESTFul Services exposed by exchange
+ * This class is used for testing the RESTFul Services exposed by exchange. The tests should be commented when
+ * being run via maven. This can be used after deploying the tests to Tomcat to see if the order is correct or not
  *
  *  User: rixon
  * Date: 14/8/13
@@ -21,6 +23,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class OrderServiceTests {
 
     @Test
+    @Ignore
     public void testRestfulServiceForValidOrder() {
         String orderURL = "http://localhost:8080/exchange/order";
         Order mockOrder = JSON.parseObject(TestUtil.fileContentAsString("newSingleOrder.json"),Order.class);
@@ -31,7 +34,8 @@ public class OrderServiceTests {
         assertThat("OrderResponse is not matching",expectedOrderResponse,is(actualOrderResponse));
     }
 
-    //@Test
+    @Test
+    @Ignore
     public void testGetOrderById() {
         String orderURL = "http://localhost:8080/exchange/order";
         OrderResponse actualOrderResponse = new RestTemplate().getForObject(orderURL,OrderResponse.class);
