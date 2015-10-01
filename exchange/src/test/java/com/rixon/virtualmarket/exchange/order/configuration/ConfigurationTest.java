62189ea026e61ee728a64db608eea0f44c460f27
Rixon Mathew <rixonmathew@gmail.com>
2015-10-01 20:04:04 +0530

    Added nasdaq company list to data provider

--- exchange/src/test/java/com/rixon/virtualmarket/exchange/order/configuration/ConfigurationTest.java
+++ exchange/src/test/java/com/rixon/virtualmarket/exchange/order/configuration/ConfigurationTest.java
@@ -10,12 +10,6 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import static junit.framework.TestCase.assertNotNull;
 
-/**
- * This class is used for validating the context configuration
- * User: rixon
- * Date: 21/8/13
- * Time: 11:26 AM
- */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(value = "classpath:applicationContext.xml")
 public class ConfigurationTest {
