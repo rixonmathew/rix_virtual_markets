e135b92f3e95becd4fb6997b7a2ab5eb5dbdbba1
Rixon Mathew <rixonmathew@gmail.com>
2016-01-01 18:15:09 +0530

    added config and eureka service

--- broker/src/main/java/com/rixon/virtualmarket/broker/BrokerApplication.java
+++ broker/src/main/java/com/rixon/virtualmarket/broker/BrokerApplication.java
@@ -3,12 +3,14 @@ package com.rixon.virtualmarket.broker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class BrokerApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(BrokerApplication.class,args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(BrokerApplication.class, args);
+  }
 
 }
